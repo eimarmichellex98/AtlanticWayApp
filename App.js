856a7c5a68c5b90a8c1e8a305a86cd9b148a421a
eimarmichellex98 <48323438+eimarmichellex98@users.noreply.github.com>
2021-04-06 11:59:01 +0100

    Add files via upload
    Home page set up done, need to figure navigating

--- App.js
+++ App.js
@@ -1,41 +1,10 @@
-//Eimar Michelle Quinn - EMQ 
-import React, {useState} from 'react';
-import { StyleSheet, Text, View, Button } from 'react-native';
+import React from 'react';
+import Home from './screens/home';
+import Navigator from './routes/homeStack';
 
-//root component of app
+//root component of app, navigates between screens
 export default function App() {
-  //state function & button function further down
-  const [name, setName] = useState('Great Atlantic Way');
   return (
-    <View style={styles.container}>
-      <View style={styles.header}>
-        <Text stlye={styles.boldText}>Welcome to the {name}</Text>
-      </View>
-      <View style={styles.buttonContainer}>
-        <Button title='Interactive Map'/><br/>
-        <Button title='Checklist'/><br/>
-        <Button title='Pedometer'/><br/>
-        <Button title='Reviews'/>
-      </View>
-    </View>
-  );
+   <Home/>
+  )
 }
-//stylesheet 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-  header: {
-    backgroundColor: 'blue',
-    padding: 20,
-  },
-  boldText: {
-    fontWeight: 'bold',
-  },
-  buttonContainer: {
-    marginTop: 10,
-  }
-});
